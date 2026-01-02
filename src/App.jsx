913fc90fc50fc303f52a4ba461829d7c0cbc44b4
jonnadulajayalakshmi73 <jonnadulajayalakshmi73@gmail.com>
2026-01-02 10:50:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App(){
+  return(
+    <div className='app'>
+      <div className='rakesh'>
+      <h1>My info: </h1>
+      <h2>Name:jaya</h2>
+      <h2>Program:B-tech.-cse</h2>
+      <h2>Section:S03</h2>
+      <h2>Course:Front End Development Frameworks and UIEngineering</h2>
+    </div>
+    </div>
+  );
+}
+export default App;
